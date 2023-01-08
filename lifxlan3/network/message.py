3b00b7bec6480695f539ee6bb9aea7bd95cce104
sguleff <sam.guleff@gmail.com>
2023-01-07 22:34:42 -0500

    updated fix for bitstream 4.0.1 defect

--- lifxlan3/network/message.py
+++ lifxlan3/network/message.py
@@ -79,7 +79,7 @@ class Message(object):
         reserved_48_format = self.frame_addr_format[1]
         response_flags_format = self.frame_addr_format[2]
         seq_num_format = self.frame_addr_format[3]
-        mac_addr = little_endian(bitstring.pack(mac_addr_format, convert_MAC_to_int(self.target_addr)))
+        mac_addr = little_endian('uint:' + bitstring.pack(mac_addr_format, convert_MAC_to_int(self.target_addr)))
         reserved_48 = little_endian(bitstring.pack(reserved_48_format, self.reserved))
         response_flags = little_endian(
             bitstring.pack(response_flags_format, self.reserved, self.ack_requested, self.response_requested))
